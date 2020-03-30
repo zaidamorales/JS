fbe0b489831b90d78f7311247e8bae740c084015
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2020-03-30 13:45:18 +0200

    Add files via upload

---
+++ Act04/js/act04.js
@@ -0,0 +1,83 @@
+var numero_uno = 10;
+var numero_dos = 20;
+var numero_tres = 30;
+var numero_quatro = 40;
+
+//TASCA 1 Comparar totes les variables, i digues quina és  més gran de totes:
+
+if(numero_uno > numero_dos)[
+  document.write("El numero 20 es mes gran que el numero 10")
+]
+
+if(numero_tres < numero_quatro)[
+  document.write("<br>El numero 40 es mes gran que el numero 30</br>")
+]
+
+if(numero_dos < numero_quatro)[
+  document.write("El numero 40 es mes gran que el numero 20")
+]
+
+if(numero_uno < numero_tres)[
+  document.write("<br>El numero 30 es mes gran que el numero 10</br>")
+]
+
+// La variable mes gran:var numero_quatro= 40
+
+//TASCA 2 Comparar totes les variables, i digues quina és  més petita de totes:
+
+var primera = 30;
+var segona = 20;
+var tercera = 10;
+var quarta = 40;
+
+
+if(quarta > primera)[
+  document.write("<br>El numero 30 es mes petit que el numero 40</br>")
+]
+
+if(primera > segona)[
+document.write("El numero 20 es mes petit que el numero 30")
+]
+
+if(segona > tercera)[
+  document.write("<br>El numero 20 es mes petit que el numero 10</br>")
+]
+
+if(quarta > tercera)[
+  document.write("El numero 10 es mes petit que el numero 40")
+]
+
+if(primera)[
+document.write("<br></br>")
+]
+// La variable mes petita: var tercera = 10
+
+//TASCA 3 Comparar totes les variables, i digues quines són iguals (si n’hi ha alguna):
+
+var uno = 10;
+var dos = 20;
+var tres = 30;
+var quatro = 40;
+
+if (primera==segona ) {
+    document.write ("<br>El numero 10 es igual que 20</br>")}
+    else {
+    document.write ("<br>El numero 10 no es igual que 20</br>")}
+
+if (primera==tercera ) {
+    document.write ("El numero 10 es igual que 30")}
+    else {
+    document.write ("El numero 10 no es igual que 30")}
+
+if (primera==quarta ) {
+    document.write ("<br>El numero 10 es igual que 40</br>")}
+    else {
+    document.write ("<br>El numero 10 no es igual que 40</br>")}
+
+if (segona==primera ) {
+    document.write ("El numero 20 es igual que 10")}
+    else {
+    document.write ("El numero 20 no es igual que 10")}
+
+// En aquestes no hi ha ninguna variable que sigui igual.
+// Tots son valors diferents.
