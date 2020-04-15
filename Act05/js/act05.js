cd5cc8cac02ab9fbe88217b9580ac911be059d62
zaidamorales <60615803+zaidamorales@users.noreply.github.com>
2020-04-15 13:08:33 +0200

    Update act05.js

--- Act05/js/act05.js
+++ Act05/js/act05.js
@@ -24,25 +24,17 @@ document.write ("</br>");
 
 //TASCA 3
 //Imprimeix totes les taules de multiplicar, utilitzant dos o més bucles.
+var m= 1;
+var b =1;
 
-function generarTaules()
-{
- var numTaules=document.getElementById('genera').value;
- document.write('<table>');
- for (var i=1; i<=10; i++)
- {
- document.write('</tr>');
-  for (var j=1; j<=numTaules; j++)
- {
- document.write('<td>');
- document.write(j + " x " + i + " = "+i*j);
- document.write('</td>');
- document.write('<td>     </td>');
- }
- document.write('</tr>');
- }
- document.write('</table>');
- }
- document.write('¿Fins quina taula vols generar?');
- document.write('<input type="text" name="genera", id="genera" />');
- document.write('<input type="button" value="Genera" onclick="generarTaules()"/>');
+for (n =0; b <= 101; n++) {
+  document.write(m + " x " +n+ " = " +m*n);
+  b++
+  document.write("</br>")
+
+  if (n == 10) {
+    document.write("</br>");
+    n = 0;
+    m++;
+  }
+}
